fc439eb5644603e0fd5667b2683ef9d81766026b
Natnael Mekonnen <naty@nany.zte.com.cn>
2023-05-30 14:11:26 -0400

    add transaction data

---
+++ Controller/Company/GetTransaction.js
@@ -0,0 +1,52 @@
+const { insertMany } = require('../../Model/Company')
+const company=require('../../Model/Company')
+const purchasedStock=require('../../Model/PurchasedStock')
+exports.getTransaction=(req,res)=>{
+    let monthdata=[0,0,0,0,0,0,0,0,0,0,0,0]
+    purchasedStock.findOne({companyName:req.body.companyName})
+    .then((data)=>{
+        if(data){
+           purchasedStock.aggregate([
+         
+            {
+            $match:{
+                createdAt:{
+                    $gte:new Date('2023-01-26T10:16:45.810+00:00'),
+                    $lt: new Date('2023-10-26T10:16:45.810+00:00'),
+                },
+                companyName:req.body.companyName,
+            }
+           },{
+            $group:{
+                _id:{$month:'$createdAt'},
+                data:{$push:'$$ROOT'}
+            }
+           },
+           {
+            $sort:{_id:1}
+           }  
+        ]).then((data)=>{
+            if(data){
+                data.map((item)=>{
+                    let total=0
+                    item.data.map((value)=>{
+                      total=total+(value.price)
+                    })
+            
+                monthdata[item._id -1]=total
+              })
+              res.status(200).json({
+                message:monthdata
+              })
+
+            }
+        })
+            
+        
+        }
+    })
+
+  
+
+
+}
