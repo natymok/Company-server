11e239dd4e535fb63ec627cf0fd395ee34db1332
Natnael Mekonnen <naty@nany.zte.com.cn>
2023-06-07 06:49:19 -0400

    add chapa  functionaity

--- Controller/Company/StockPrice.js
+++ Controller/Company/StockPrice.js
@@ -1,37 +1,45 @@
 const PurchasedStock=require('../../Model/PurchasedStock')
+const company=require('../../Model/Company')
 exports.getStockPrice=(req,res)=>{
 
-
-        PurchasedStock.find({companyName:req.body.companyName}).then((data)=>{
-          if(data){
-          
-            let total=0
-         
-            data.map((item)=>{
-          
-               total=parseInt(item.price) + total
-              
-              
+       company.findOne({companyName:req.body.companyName})
+       .then((value)=>{
+        if(value){
+          PurchasedStock.find({companyName:req.body.companyName}).then((data)=>{
+            if(data){
                
-
-            })
-            res.status(200).json({
-              message:total,
-             
-              customers:data.length
-
-
-
-            })
-         
-          
-          }
-        })
-        .catch((err)=>{
-            res.status(400).json({
-                error:err
-            })
+            
+              let total=0
+           
+              data.map((item)=>{
+            
+                 total=parseInt(item.price) + total
+                
+                
+                 
+  
+              })
+              res.status(200).json({
+                message:total,
+               
+                customers:data.length,
+                balance:value.balance
+  
+  
+  
+              })
+           
+            
+            }
           })
+          .catch((err)=>{
+              res.status(400).json({
+                  error:err
+              })
+            })
+        }
+       })
+        
   
 
 }
