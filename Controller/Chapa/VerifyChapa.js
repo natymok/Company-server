5016f9966355ab8a12c0d7a17fc26b12dce655e7
Natnael Mekonnen <naty@nany.zte.com.cn>
2023-06-07 08:58:46 -0400

    update

--- Controller/Chapa/VerifyChapa.js
+++ Controller/Chapa/VerifyChapa.js
@@ -1,5 +1,13 @@
 const company=require('../../Model/Company')
 exports.Vchapa=(req,res)=>{
-    req.params.body[0]
+    const data = req.params.body.split(",")
+    company.findOne({companyEmail:data[1]})
+    .then((item)=>{
+       
+        company.findOneAndUpdate({companyEmail:data[1]},{balance:item.balance - parseInt(data[0])},{new:true})
+        .then((ittem)=>{
+            console.log(ittem)
+        })
+    })
 
 }
