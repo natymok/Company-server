5c93a0b9bdf665701717b532c28b23e112ad1e19
Natnael Mekonnen <naty@nany.zte.com.cn>
2023-06-08 01:07:33 -0400

    update

--- index.server.js
+++ index.server.js
@@ -4,6 +4,7 @@ const bodyParser=require('body-parser')
 const mongoose=require('mongoose')
 const userRoute=require('./Routes/userRoutes')
 const cors=require('cors')
+const path =require('path')
 require('dotenv').config()
 mongoose.set('strictQuery', true);
 mongoose.connect(process.env.MONGODB_URI,{ useNewUrlParser: true ,      useUnifiedTopology: true,}).
@@ -31,6 +32,10 @@ else{
       res.send('api running')
   }))
 }
+app.use(express.static(path.join(__dirname, 'build')));
+app.get('/*', function(req, res) {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
 
 app.listen(process.env.PORT || 3000,()=>{
   console.log('server listineang on port 3000')
